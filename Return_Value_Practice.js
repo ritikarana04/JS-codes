dcd0b68642e65418a830e08cabda61806ae37822
Ritika Rana <ranaritu414@gmail.com>
2023-07-27 06:23:58 +0530

    Create Return_Value_Practice.js

---
+++ Return_Value_Practice.js
@@ -0,0 +1,14 @@
+//It's time to practice returning values from a function! Write a simple function multiply which accepts two numerical arguments and returns their product (multiply them together).  Make sure to return the value instead of printing it!
+
+//multiply(2,3) 
+//multiply(9,9) 
+//multiply(5,4) 
+
+// DEFINE YOUR FUNCTION BELOW:
+
+
+function multiply (x,y){
+    return x*y;//return 
+    
+}
+
