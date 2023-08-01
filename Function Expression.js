1b9b530ecff6b3d943bb9a99cd51ea8503cdabf9
Ritika Rana <ranaritu414@gmail.com>
2023-08-01 12:59:36 +0530

    Create Function Expression.js

---
+++ Function Expression.js
@@ -0,0 +1,8 @@
+//Define a function that returns the square of a number. Define the function as a function expression, stored in a variable called square. 
+
+const square=function(num){
+    return num*num
+
+}
+
+square(7);
